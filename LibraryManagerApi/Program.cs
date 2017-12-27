d11b3b72a71a00fbac6164cd01108b58a6f803f8
Sam Smith <sam@example.com>
2017-12-27 11:13:21 +0000

    Added ability to extract the secure LibraryDatabaseConnectionString from Azure

--- LibraryManagerApi/Program.cs
+++ LibraryManagerApi/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace LibraryManagerApi
 {
     public class Program
     {
+        private static string GetKeyVaultEndpoint() => Environment.GetEnvironmentVariable("AZURE_LIBRARY_VAULT");
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
@@ -19,6 +19,19 @@ namespace LibraryManagerApi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var keyVaultEndpoint = GetKeyVaultEndpoint();
+                    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+                        builder.AddAzureKeyVault(
+                            keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
