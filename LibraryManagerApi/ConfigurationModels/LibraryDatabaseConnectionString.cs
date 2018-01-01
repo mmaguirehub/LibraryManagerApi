fbbd5ec782a0ed41c298a9c789357afa7cbe86c5
M E Maguire <mark.maguire1979@gmail.com>
2018-01-01 20:48:41 +0000

    Resetting

---
+++ LibraryManagerApi/ConfigurationModels/LibraryDatabaseConnectionString.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace LibraryManagerApi.ConfigurationModels
+{
+    public class LibraryDatabaseConnectionString
+    {
+        public string Value { get; set; }
+
+        public LibraryDatabaseConnectionString()
+        {
+        }
+
+        private LibraryDatabaseConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("LibraryDatabaseConnectionString");
+            Value = connectionString.Value;
+        }
+
+        public static LibraryDatabaseConnectionString FromConfiguration(IConfiguration configuration)
+        {
+            return new LibraryDatabaseConnectionString(configuration);
+        }
+
+        public static implicit operator string(LibraryDatabaseConnectionString libraryDatabaseConnectionString)
+        {
+            return libraryDatabaseConnectionString.Value;
+        }
+    }
+}
